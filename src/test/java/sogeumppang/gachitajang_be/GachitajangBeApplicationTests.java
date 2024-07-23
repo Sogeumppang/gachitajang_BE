e42a6fe030ecb5a61031fd2a06f6744c8cf11ab1
je326 <mya326@naver.com>
2024-07-23 23:47:46 +0900

    initial commit

---
+++ src/test/java/sogeumppang/gachitajang_be/GachitajangBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sogeumppang.gachitajang_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachitajangBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
