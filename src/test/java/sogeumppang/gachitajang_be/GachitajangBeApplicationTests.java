46ca23d71b60ad062d5f0b8d74853a75ca3ee139
je326 <mya326@naver.com>
2024-09-03 03:08:07 +0900

    in-memory DB 명시

--- src/test/java/sogeumppang/gachitajang_be/GachitajangBeApplicationTests.java
+++ src/test/java/sogeumppang/gachitajang_be/GachitajangBeApplicationTests.java
@@ -3,7 +3,7 @@ package sogeumppang.gachitajang_be;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class GachitajangBeApplicationTests {
 
     @Test
