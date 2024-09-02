534da5c1ad7c6349585c98b5bde9925aec203675
je326 <mya326@naver.com>
2024-09-03 01:54:46 +0900

    JWT property 설정

---
+++ src/main/java/sogeumppang/gachitajang_be/jwt/config/JwtConfiguration.java
@@ -0,0 +1,22 @@
+package sogeumppang.gachitajang_be.jwt.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtConfiguration {
+    private String secret;
+
+    private Long accessExpiration;
+
+    private Long refreshExpiration;
+
+    private String accessHeader;
+
+    private String refreshSetCookie;
+
+    private Long cookieExpiration;
+}
